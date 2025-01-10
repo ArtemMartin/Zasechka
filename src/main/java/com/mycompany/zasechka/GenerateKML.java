ffe36cd88cdd28590e970643f1529addaf778b0b
Артем@LYCH-PK <arcenivashenko@yandex.ru>
2025-01-10 16:30:08 +0300

    изменен способ расчета BL XY

--- src/main/java/com/mycompany/zasechka/GenerateKML.java
+++ src/main/java/com/mycompany/zasechka/GenerateKML.java
@@ -4,12 +4,16 @@
  */
 package com.mycompany.zasechka;
 
+import org.osgeo.proj4j.*;
+
 import static com.mycompany.zasechka.Zasechka.getTime;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -53,9 +57,9 @@ public class GenerateKML {
     }
 
     public void generate() {
-        double[] bl = getBLPoint(x, y);
-        double b = bl[0];
-        double l = bl[1];
+        List<Double> bl = refactorXYtoBL(x, y);
+        double b = bl.get(0);
+        double l = bl.get(1);
 
         try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(
                 new FileOutputStream(filePath), "UTF-8"))) {
@@ -91,34 +95,63 @@ public class GenerateKML {
         }
 
     }
-
-    public double[] getBLPoint(double x, double y) {
+    public static List<Double> refactorXYtoBL(double x, double y) {
+        List<Double> list = new ArrayList();
         if (x < 99999) {
             x += 5300000;
         }
         if (y < 99999) {
             y += 7300000;
         }
-        x += getDX(y);
-        y += -117.0;
-        int nZonu = (int) Math.round(y * Math.pow(10.0, -6.0));
-        double b = x / 6367558.4968;
-        double B0 = b + Math.sin(2.0 * b) * (0.00252588685 - 1.49186E-5 * Math.pow(Math.sin(b), 2.0) + 1.1904E-7 * Math.pow(Math.sin(b), 4.0));
-        double z0 = (y - (double) (10 * nZonu + 5) * Math.pow(10.0, 5.0)) / (6378245.0 * Math.cos(B0));
-        double dB = -Math.pow(z0, 2.0) * Math.sin(2.0 * B0) * (0.251684631 - 0.003369263 * Math.pow(Math.sin(B0), 2.0) + 1.1276E-5 * Math.pow(Math.sin(B0), 4.0) - Math.pow(z0, 2.0) * 0.10500614 - 0.04559916 * Math.pow(Math.sin(B0), 2.0) + 0.00228901 * Math.pow(Math.sin(B0), 4.0) - 2.987E-5 * Math.pow(Math.sin(B0), 6.0) - Math.pow(z0, 2.0) * (0.042858 - 0.025318 * Math.pow(Math.sin(B0), 2.0) + 0.014346 * Math.pow(Math.sin(B0), 4.0) - 0.001264 * Math.pow(Math.sin(B0), 6.0) - Math.pow(z0, 2.0) * (0.01672 - 0.0063 * Math.pow(Math.sin(B0), 2.0) + 0.01188 * Math.pow(Math.sin(B0), 4.0) - 0.00328 * Math.pow(Math.sin(B0), 6.0))));
-        double l = z0 * (1.0 - 0.0033467108 * Math.pow(Math.sin(B0), 2.0) - 5.6002E-6 * Math.pow(Math.sin(B0), 4.0) - 1.87E-8 * Math.pow(Math.sin(B0), 6.0) - Math.pow(z0, 2.0) * (0.16778975 + 0.16273586 * Math.pow(Math.sin(B0), 2.0) - 5.249E-4 * Math.pow(Math.sin(B0), 4.0) - 8.46E-6 * Math.pow(Math.sin(B0), 6.0) - Math.pow(z0, 2.0) * (0.0420025 + 0.1487407 * Math.pow(Math.sin(B0), 2.0) + 0.005942 * Math.pow(Math.sin(B0), 4.0) - 1.5E-5 * Math.pow(Math.sin(B0), 6.0) - Math.pow(z0, 2.0) * (0.01225 + 0.09477 * Math.pow(Math.sin(B0), 2.0) + 0.03282 * Math.pow(Math.sin(B0), 4.0) - 3.4E-4 * Math.pow(Math.sin(B0), 6.0) - Math.pow(z0, 2.0) * (0.0038 + 0.0524 * Math.pow(Math.sin(B0), 2.0) + 0.0482 * Math.pow(Math.sin(B0), 4.0) + 0.0032 * Math.pow(Math.sin(B0), 6.0))))));
-        double B = (B0 + dB) * 180.0 / Math.PI;
-        double L = (6.0 * ((double) nZonu - 0.5) / 57.29577951 + l) * 180.0 / Math.PI;
+// Создаем исходную и целевую системы координат
+        CRSFactory factory = new CRSFactory();
+        CoordinateReferenceSystem srcCRS = factory.createFromName("EPSG:28407");
+        CoordinateReferenceSystem dstCRS = factory.createFromName("EPSG:4326");
 
-        return new double[]{B, L};
-    }
+        // Создаем объект для преобразования координат
+        CoordinateTransformFactory ctFactory = new CoordinateTransformFactory();
+        CoordinateTransform transform = ctFactory.createTransform(srcCRS, dstCRS);
 
-    public double getDX(double y) {
-        double srY = 7423378.5;
-        double koef = 339.0 / 140633.0;
-        double dX = (y - srY) * koef + (-59.0);
-        return dX;
+        // Преобразуем координаты
+        //сначала вводим долготу потом широту
+        ProjCoordinate srcCoord = new ProjCoordinate(y, x);
+        ProjCoordinate dstCoord = new ProjCoordinate();
+        transform.transform(srcCoord, dstCoord);
+
+        // Выводим результат(наоборот x->y)
+        //System.out.println("Преобразованные координаты: " + dstCoord.x + ", " + dstCoord.y);
+        //возвращаем масив b, l
+        list.add(dstCoord.y);
+        list.add(dstCoord.x);
+        return list;
     }
+//    public double[] getBLPoint(double x, double y) {
+//        if (x < 99999) {
+//            x += 5300000;
+//        }
+//        if (y < 99999) {
+//            y += 7300000;
+//        }
+//        x += getDX(y);
+//        y += -117.0;
+//        int nZonu = (int) Math.round(y * Math.pow(10.0, -6.0));
+//        double b = x / 6367558.4968;
+//        double B0 = b + Math.sin(2.0 * b) * (0.00252588685 - 1.49186E-5 * Math.pow(Math.sin(b), 2.0) + 1.1904E-7 * Math.pow(Math.sin(b), 4.0));
+//        double z0 = (y - (double) (10 * nZonu + 5) * Math.pow(10.0, 5.0)) / (6378245.0 * Math.cos(B0));
+//        double dB = -Math.pow(z0, 2.0) * Math.sin(2.0 * B0) * (0.251684631 - 0.003369263 * Math.pow(Math.sin(B0), 2.0) + 1.1276E-5 * Math.pow(Math.sin(B0), 4.0) - Math.pow(z0, 2.0) * 0.10500614 - 0.04559916 * Math.pow(Math.sin(B0), 2.0) + 0.00228901 * Math.pow(Math.sin(B0), 4.0) - 2.987E-5 * Math.pow(Math.sin(B0), 6.0) - Math.pow(z0, 2.0) * (0.042858 - 0.025318 * Math.pow(Math.sin(B0), 2.0) + 0.014346 * Math.pow(Math.sin(B0), 4.0) - 0.001264 * Math.pow(Math.sin(B0), 6.0) - Math.pow(z0, 2.0) * (0.01672 - 0.0063 * Math.pow(Math.sin(B0), 2.0) + 0.01188 * Math.pow(Math.sin(B0), 4.0) - 0.00328 * Math.pow(Math.sin(B0), 6.0))));
+//        double l = z0 * (1.0 - 0.0033467108 * Math.pow(Math.sin(B0), 2.0) - 5.6002E-6 * Math.pow(Math.sin(B0), 4.0) - 1.87E-8 * Math.pow(Math.sin(B0), 6.0) - Math.pow(z0, 2.0) * (0.16778975 + 0.16273586 * Math.pow(Math.sin(B0), 2.0) - 5.249E-4 * Math.pow(Math.sin(B0), 4.0) - 8.46E-6 * Math.pow(Math.sin(B0), 6.0) - Math.pow(z0, 2.0) * (0.0420025 + 0.1487407 * Math.pow(Math.sin(B0), 2.0) + 0.005942 * Math.pow(Math.sin(B0), 4.0) - 1.5E-5 * Math.pow(Math.sin(B0), 6.0) - Math.pow(z0, 2.0) * (0.01225 + 0.09477 * Math.pow(Math.sin(B0), 2.0) + 0.03282 * Math.pow(Math.sin(B0), 4.0) - 3.4E-4 * Math.pow(Math.sin(B0), 6.0) - Math.pow(z0, 2.0) * (0.0038 + 0.0524 * Math.pow(Math.sin(B0), 2.0) + 0.0482 * Math.pow(Math.sin(B0), 4.0) + 0.0032 * Math.pow(Math.sin(B0), 6.0))))));
+//        double B = (B0 + dB) * 180.0 / Math.PI;
+//        double L = (6.0 * ((double) nZonu - 0.5) / 57.29577951 + l) * 180.0 / Math.PI;
+//
+//        return new double[]{B, L};
+//    }
+//
+//    public double getDX(double y) {
+//        double srY = 7423378.5;
+//        double koef = 339.0 / 140633.0;
+//        double dX = (y - srY) * koef + (-59.0);
+//        return dX;
+//    }
 
     /*
     KT1 5389480 7423290
